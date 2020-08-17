ccc499558cdaf879c02551ff6b720ccf7b560418
ravikiran-pro <proraviki@gmail.com>
2020-08-17 17:02:53 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          welcome to the party
+          welcome to the party this is me 
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
